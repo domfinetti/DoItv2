b65087e6c803231f697413a3b0d03beda3547522
Dominick Dayton Finetti <dom.finetti93@gmail.com>
2016-11-01 20:38:32 -0500

    added tasks & important emojis

---
+++ DoItv2/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoItv2
+//
+//  Created by Dominick Dayton Finetti on 11/1/16.
+//  Copyright © 2016 Zappy Code. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+var important = false
+
+}
--- DoItv2/ViewController.swift
+++ DoItv2/ViewController.swift
@@ -12,23 +12,50 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
+    var tasks: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
+        tasks = makeTasks()
+        
         tableView.dataSource = self
         tableView.delegate = self
 
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return  tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Hello"
-        return cell
+        let task = tasks[indexPath.row]
+        
+        if task.important {
+            cell.textLabel?.text = "❗️\(task.name)"
+        }else{
+            
+            cell.textLabel?.text = task.name
+        }
+            return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Feed Zoe Kai"
+        task1.important = true
+        
+        let task2 = Task()
+        task2.name = "E-mail boss"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Take out the trash"
+        task3.important = false
+        
+        return [task1, task2, task3]
     }
     
 }
